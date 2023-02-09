55bc276f7226d8635a4634ac1dd5aad22c0b31a2
Chstphr <somto565@gmail.com>
2023-02-09 16:21:14 +0100

    still setting up

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from './Head/Head'
+import Navbar from './NavBar/Navbar'
+import Serach from './Search/Serach'
+
+const Header = () => {
+  return (
+    <>
+    <Head />
+    <Serach />
+    <Navbar />
+    </>
+  )
+}
+
+export default Header
