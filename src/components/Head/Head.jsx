a6dfb10f94fb0f682e8234be3efb030efff4c033
Chstphr <somto565@gmail.com>
2023-02-09 16:56:56 +0100

    imported google fonts

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 const Head = () => {
   return (
     <>
+    <section className="head">
+        <div className="container d_flex">
+            <div className="left row">
+                <i className="fa fa-phone"></i>
+            </div>
+        </div>
+    </section>
     </>
   )
 }
