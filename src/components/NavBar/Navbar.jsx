76b62fbe375dda5d6ed536866a47c3683f3f418a
Chstphr <somto565@gmail.com>
2023-02-12 08:11:29 +0100

    navbar construction

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <header className='header'>
+      <div className="container d_flex">
+        <div className="categories d_flex">
+          <span className="fa-solid fa-border-all"></span>
+            <h4>
+              Categories <i className="fa fa-chevron-down"></i>
+            </h4>
+        </div>
+        <div className="navlink">
+          <ul className="nav">
+            <li>
+              <link></link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    </>
   )
 }
 
