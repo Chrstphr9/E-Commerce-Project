7c63cac514d07dcb27e64e04bfdb2faabb7df06d
Chstphr <somto565@gmail.com>
2023-02-20 19:35:46 +0100

    sorted the icon

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,56 +1,57 @@
-import React from 'react'
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
-  const [mobileMenu, setMobileMenu] = useState(false)
+  const [mobileMenu, setMobileMenu] = useState(false);
 
   return (
     <>
-    
-    <header className='header'>
-      <div className="container d_flex">
-        <div className="categories d_flex">
-          <span className="fa-solid fa-border-all"></span>
+      <header className="header">
+        <div className="container d_flex">
+          <div className="categories d_flex">
+            <span className="fa-solid fa-border-all"></span>
             <h4>
               Categories <i className="fa fa-chevron-down"></i>
             </h4>
-        </div>
-        <div className="navLink">
-          <ul className="nav">
-            <li>
-              <Link to="/">home</Link>
-            </li>
-            <li>
-              <Link to="/pages">pages</Link>
-            </li>
-            <li>
-              <Link to="/user">user account</Link>
-            </li>
-            <li>
-              <Link to="/vendor">vendor account</Link>
-            </li>
-            <li>
-              <Link to="/track">track my order</Link>
-            </li>
-            <li>
-              <Link to="/contact">contact</Link>
-            </li>
-          </ul>
+          </div>
+          <div className="navLink">
+            <ul className="nav">
+              <li>
+                <Link to="/">home</Link>
+              </li>
+              <li>
+                <Link to="/pages">pages</Link>
+              </li>
+              <li>
+                <Link to="/user">user account</Link>
+              </li>
+              <li>
+                <Link to="/vendor">vendor account</Link>
+              </li>
+              <li>
+                <Link to="/track">track my order</Link>
+              </li>
+              <li>
+                <Link to="/contact">contact</Link>
+              </li>
+            </ul>
 
-          <button className="toggle" onClick={() => setMobileMenu(!mobileMenu)}>
-            {
-              mobileMenu ? <i className="fas fa-times close home-btn"></i> :
-              <i className="fas fa-bars open"></i>
-            }
-          </button>
-
+            <button
+              className="toggle"
+              onClick={() => setMobileMenu(!mobileMenu)}
+            >
+              {mobileMenu ? (
+                <i className="fa fa-times close home-btn"></i>
+              ) : (
+                <i className="fa fa-bars open"></i>
+              )}
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
