7a5072a19482ea5c1bf03d9ea1a1070cee4411ec
Chstphr <somto565@gmail.com>
2023-02-28 11:00:32 +0100

    called the link dependecy

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../assets/images/logo.svg'
+import { Link } from 'react-router-dom'
 
 
 const Search = () => {
@@ -9,6 +10,23 @@ const Search = () => {
       <div className="container c_flex">
         <div className="logo width">
           <img src={logo} alt="" />
+        </div>
+
+        <div className="search-box f_flex">
+          <i className="fa fa-search">
+            <input type="text" placeholder='Search and hit enter...' />
+            <span>All Category</span>
+          </i>
+        </div>
+
+        <div className="icon f_flex width">
+          <i className="fa fa-user icon_circle">
+            <div className="cart">
+              <Link to='/cart'>
+                <i className="fa fa-shopping-bag icon-circle"></i>
+              </Link>
+            </div>
+          </i>
         </div>
       </div>
     </section>
