af26739fe6191b909dcdc2232c2b12c857db2697
Chstphr <somto565@gmail.com>
2023-03-04 09:12:10 +0100

    destructred the project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from'react';
 import './App.css';
 import { BrowserRouter as Router, Routes} from 'react-router-dom'
-import Header from './components/Header';
+
 
 function App() {
   return (
     <>
-    <Router>
-      <Header />
-      <Routes>
-        {/* <Route path='/' exact>
-          <About />
-        </Route> */}
-      </Routes>
-    </Router>
+   
     </>
   );
 }
