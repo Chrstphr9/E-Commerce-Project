ec42e118ef8ff8991974c1ae57f886cdd6077a29
Chstphr <somto565@gmail.com>
2023-03-04 09:37:44 +0100

    setting the header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 
 
 function App() {
