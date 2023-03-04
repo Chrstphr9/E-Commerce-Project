839bd34d9bf9abb46048be40088e5c7ff1fc6578
Chstphr <somto565@gmail.com>
2023-03-04 09:24:24 +0100

    initial routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from'react';
 import './App.css';
-import { BrowserRouter as Router, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Link} from 'react-router-dom'
 
 
 function App() {
   return (
     <>
-   
+   <Router>
+    <Header />
+    <Routes>
+      <Route exact path='/' component={Home} />
+    </Routes>
+   </Router>
     </>
   );
 }
