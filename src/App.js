4f1a1a8e152d8eb1376107ce83bf5ef0eb3e766f
Chstphr <somto565@gmail.com>
2023-03-05 21:39:52 +0100

    still setting up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <>
    <Router>
     <Header />
+
     <Routes>
       <Route exact path='/' component={Home} />
     </Routes>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+// import Medcine from '../../assets/images/Medcine.svg'
+// import { AiOutlineSearch} from 'react-icons/ai'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
