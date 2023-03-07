e476b31f44b4ae5b9dffa5c6d09c6f2497c7ae5e
Chstphr <somto565@gmail.com>
2023-03-07 20:08:47 +0100

    still setting up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './header.css'
-// import Medcine from '../../assets/images/Medcine.svg'
-// import { AiOutlineSearch} from 'react-icons/ai'
+import Medicine from '../../assets/images/Medicine.svg'
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const Header = () => {
+  window.addEventListener("scroll", function() {
+    const header = this.document.querySelector(".header")
+    header.classList.toggle("active", this.window.scrollY > 100)
+  })
+  window.scrollTo({ top: document.body.scrollHeight, behaviour: "smooth"})
+  
   return (
-    <div>Header</div>
+    <header className='header'>
+    <div className="scontainer flex">
+      <div className="logo">
+        <img src={Medicine} alt="Medicine" />
+      </div>
+      <div className="search flex">
+        < AiOutlineSearch className='searchIcon'/>
+        <input type="text" placeholder='Search' />
+      </div>
+    </div>
+  </header>
   )
 }
 
